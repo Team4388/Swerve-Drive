eebb620e38603f775c8bfd608d99ed23cdf44e85
aarav18 <aaravshah18@gmail.com>
2021-12-02 17:56:35 -0700

    pid

--- src/main/java/frc4388/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc4388/robot/subsystems/SwerveDrive.java
@@ -82,9 +82,11 @@ public class SwerveDrive
         
         m_leftFrontWheelMotor.set(m_leftFrontSteerMotor.get() + leftFront.speedMetersPerSecond * SwerveDriveConstants.WHEEL_SPEED);
         m_rightFrontWheelMotor.set(m_rightFrontSteerMotor.get() + rightFront.speedMetersPerSecond * SwerveDriveConstants.WHEEL_SPEED);
-        m_leftBackWheelMotor.set(m_leftBackSteerMotor.get() + lefBack.speedMetersPerSecond * SwerveDriveConstants.WHEEL_SPEED);
+        m_leftBackWheelMotor.set(m_leftBackSteerMotor.get() + leftBack.speedMetersPerSecond * SwerveDriveConstants.WHEEL_SPEED);
         m_rightBackWheelMotor.set(m_rightBackSteerMotor.get() + rightBack.speedMetersPerSecond * SwerveDriveConstants.WHEEL_SPEED);
     
+        // PID
+        
     }
 
     // public void driveFieldRelative(double awayFromStation, double towardLeftBoundary, double rotate)
